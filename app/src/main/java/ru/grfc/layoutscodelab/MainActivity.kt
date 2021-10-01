73aea603245b6754a225619387d7739829f449c5
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-10-01 14:38:33 +0900

    Create your custom layout

--- app/src/main/java/ru/grfc/layoutscodelab/MainActivity.kt
+++ app/src/main/java/ru/grfc/layoutscodelab/MainActivity.kt
@@ -18,8 +18,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import kotlinx.coroutines.launch
@@ -56,7 +60,8 @@ fun LayoutsCodelab() {
         BodyContent(
             Modifier
                 .padding(innerPadding)
-                .padding(8.dp))
+                .padding(8.dp)
+        )
     }
 }
 
@@ -66,6 +71,39 @@ fun BodyContent(modifier: Modifier = Modifier) {
         Text(text = "Hello there!")
         Text(text = "Thanks for going through the Layouts codelab")
         ScrollingList()
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
@@ -124,14 +162,16 @@ fun ScrollingList() {
             Button(onClick = {
                 coroutineScope.launch {
                     scrollState.animateScrollToItem(0)
-                } }) {
+                }
+            }) {
                 Text("Scroll to the top")
             }
 
             Button(onClick = {
                 coroutineScope.launch {
                     scrollState.animateScrollToItem(listSize - 1)
-                } }) {
+                }
+            }) {
                 Text("Scroll to the end")
             }
         }
